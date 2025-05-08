5b31f209fe8a8fcbb0499e21e5f0ebc5546402bc
frontend_codeco <lucaspradier73@gmail.com>
2025-05-08 15:38:34 -0300

    categoria

--- resources/js/Pages/Categoria/CategoriaContacto.jsx
+++ resources/js/Pages/Categoria/CategoriaContacto.jsx
@@ -29,7 +29,7 @@ const CategoriaContacto = ({ userId }) => {
           // Handle error display to user if needed
         });
     }
-  }, [userId]);
+  }, [userId, closeModal]);
 
   
   function openModal() {
@@ -47,7 +47,7 @@ const CategoriaContacto = ({ userId }) => {
           <h3>Categorías </h3>
         </div>
         <div className="col-md-3 d-flex justify-content-end align-items-center">
-          <button className="btn btn-primary" onClick={openModal}>
+          <button className="btn btn-primary w-100" onClick={openModal}>
             Asignar Contacto
           </button>
         </div>
